8f26f98d5c178abe9cfbebdcf97b739ed7d874a7
Soubhik <soubhik960@gmail.com>
2024-07-29 14:34:02 +0530

    add more apk links

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,4 @@
 console.log("Hello World");
+
+// https://drive.google.com/file/d/1L3-j0PQVMJrUVn_TrauSY2PY5sb0KPZP/view?usp=drive_link : PratimaShopping 2.0.1
+// https://drive.google.com/file/d/1JWP3cZSpEg-tMuw062zAIhohUZOnJnaa/view?usp=drive_link : WhatsApp
